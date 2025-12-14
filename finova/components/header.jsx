24e0c762097ad562173fa21761454de1432aeedb
Hritik-Mangal <hritikmangal11@gmail.com>
2025-12-15 02:09:44 +0530

    Build app layout

---
+++ finova/components/header.jsx
@@ -0,0 +1,36 @@
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs"
+import Image from "next/image"
+import Link from "next/link"
+
+const Header = () => {
+  return (
+    <div className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
+       <nav className="container mx-auto px-4 py-4 flex items-center justify-between">
+        <Link href = '/'>
+            <Image
+            src = {"/logo.png"}
+            alt = "wlth logo"
+            height = {60} width={200}
+            className="h-12 w-auto object-contain"    
+            />
+        </Link>
+       
+          <header className="flex justify-end items-center p-4 gap-4 h-16">
+            <SignedOut>
+              <SignInButton />
+              <SignUpButton>
+                <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </SignedOut>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
+          </header>
+        </nav>
+    </div>
+  )
+}
+
+export default Header
