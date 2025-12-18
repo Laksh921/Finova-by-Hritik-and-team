186d7ec1c70e2f04e48f973267ddb56731e41c23
Ajaygrol123 <159447181+Ajaygrol123@users.noreply.github.com>
2025-12-18 14:29:03 +0530

    Update prisma.js

--- finova/lib/prisma.js
+++ finova/lib/prisma.js
@@ -1,6 +1,6 @@
 import { PrismaClient } from "./generated/prisma/client";
 
-export const db = globalThis.prisma || new PrismaClient();
+export const DATABASE = globalThis.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production"){
     globalThis.prisma = db;
@@ -9,4 +9,4 @@ if (process.env.NODE_ENV !== "production"){
 // globalThis.prisma: This global variable ensures that the Prisma client instance is
 // reused across hot reloads during development. Without this, each time your application
 // reloads, a new instance of the Prisma client would be created, potentially leading
-// to connection issues.
+// to connection issues. 
