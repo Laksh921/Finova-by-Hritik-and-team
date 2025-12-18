4cf43b33082ffcc4558a31cf5e684fecb9efe37c
Hritik-Mangal <hritikmangal11@gmail.com>
2025-12-18 14:53:33 +0530

    Prisma Resetup 2
    Fixing the version issue and code issue again
    
    Co-Authored-By: Prachi-045 <217204075+Prachi-045@users.noreply.github.com>
    Co-Authored-By: Ajaygrol123 <159447181+Ajaygrol123@users.noreply.github.com>

--- finova/lib/prisma.js
+++ finova/lib/prisma.js
@@ -1,12 +1,12 @@
-import { PrismaClient } from "./generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 
-export const db = globalThis.prisma || new PrismaClient();
+const globalForPrisma = globalThis;
 
-if (process.env.NODE_ENV !== "production"){
-    globalThis.prisma = db;
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
 }
-
-// globalThis.prisma: This global variable ensures that the Prisma client instance is
-// reused across hot reloads during development. Without this, each time your application
-// reloads, a new instance of the Prisma client would be created, potentially leading
-// to connection issues.
+export const db = prisma;
