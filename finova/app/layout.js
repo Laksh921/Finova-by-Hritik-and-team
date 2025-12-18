45e61634e3d22ade9d09beef2285fb3633f79fad
Hritik-Mangal <hritikmangal11@gmail.com>
2025-12-19 01:34:33 +0530

    Resetting prisma and making dashboard page

--- finova/app/layout.js
+++ finova/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "sonner";
 
 const inter = Inter({subsets:["latin"]});
 
@@ -14,14 +15,17 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <ClerkProvider>
-
-    
+  
     <html lang="en">
+      <head>
+          <link rel="icon" href="/logo-sm.png" sizes="any" />
+      </head>
       <body
         className={`${inter.className}`}>
         {/* header   */}
         <Header/>
         <main className="min-h-screen">{children}</main>
+        <Toaster richColors />
         
         {/* footer */}
         <footer className="bg-blue-50 py-12">
