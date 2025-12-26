53c88aa3e6f2e04f460b676ba67151664bbb47f8
Hritik-Mangal <hritikmangal11@gmail.com>
2025-12-26 17:09:10 +0530

    changed Page.jsx
    Co-Authored-By: Ajaygrol123 <159447181+Ajaygrol123@users.noreply.github.com>
    Co-Authored-By: Prachi-045 <217204075+Prachi-045@users.noreply.github.com>

--- finova/components/hero.jsx
+++ finova/components/hero.jsx
@@ -27,7 +27,7 @@ const HeroSection = () => {
 
 
   return (
-    <div className="pb-20 px-4">
+    <div className="pt-30 pb-20 px-4">
       <div className="container mx-auto text-center">
         <h1 className="text-5xl md:text-8xl lg:text-[105px] pb-6 gradient-title">
             Manage Your Finances <br /> with Intelligence
@@ -42,7 +42,7 @@ const HeroSection = () => {
                     Get Started
                 </Button>
             </Link>
-            <Link href="https://www.youtube.com/roadsidecoder">
+            <Link href="">
                 <Button size="lg" variant= 'outline' className="px-8">
                     Watch Demo
                 </Button>
